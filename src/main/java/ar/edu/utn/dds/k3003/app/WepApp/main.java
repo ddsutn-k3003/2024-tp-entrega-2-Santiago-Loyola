91e55bac9594f96aa5cc8c26c00ef59588816bc0
Santiago <santiloyola@frba.utn.edu.ar>
2024-05-05 17:55:35 -0300

    Se testea como las formas de colaborar

---
+++ src/main/java/ar/edu/utn/dds/k3003/app/WepApp/main.java
@@ -0,0 +1,4 @@
+package ar.edu.utn.dds.k3003.app.WepApp;
+
+public class main {
+}
---
+++ src/test/java/ar/edi/itn/dds/k3003/model/ColaboradorTest.java
@@ -0,0 +1,43 @@
+package ar.edi.itn.dds.k3003.model;
+
+import ar.edu.utn.dds.k3003.facades.dtos.FormaDeColaborarEnum;
+import ar.edu.utn.dds.k3003.model.Colaborador;
+import org.junit.jupiter.api.Test;
+
+import java.text.Normalizer;
+import java.util.ArrayList;
+import java.util.List;
+
+import static ar.edu.utn.dds.k3003.facades.dtos.FormaDeColaborarEnum.DONADOR;
+import static ar.edu.utn.dds.k3003.facades.dtos.FormaDeColaborarEnum.TRANSPORTADOR;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ColaboradorTest {
+
+  @Test
+  public void testColaboradorFormasDeColaborarEnumNull(){
+    List<FormaDeColaborarEnum> formaDeColaborarEnums = null;
+    Colaborador Santiago = new Colaborador("Santiago",formaDeColaborarEnums);
+
+    assertEquals(null,Santiago.getListaFormaDeColaborarEnum(),"Santiago no colabora de ninguna manera");
+
+  }
+  @Test
+  public void testColaboradorFormasDeColaborarEnumDONADOR(){
+    List<FormaDeColaborarEnum> formaDeColaborarEnumsDONADOR = new ArrayList<>();
+    formaDeColaborarEnumsDONADOR.add(DONADOR);
+    Colaborador Lucas = new Colaborador("Lucas",formaDeColaborarEnumsDONADOR);
+
+    assertEquals(formaDeColaborarEnumsDONADOR,Lucas.getListaFormaDeColaborarEnum(),"Lucas colabora como donador");
+
+  }
+  @Test
+  public void testColaboradorFormasDeColaborarEnumDONADORTRANSPORTARDOR(){
+    List<FormaDeColaborarEnum> formasDeColaborarEnumDONADORTRANSPORTADOR = new ArrayList<>();
+    formasDeColaborarEnumDONADORTRANSPORTADOR.add(DONADOR);
+    formasDeColaborarEnumDONADORTRANSPORTADOR.add(TRANSPORTADOR);
+    Colaborador Juan = new Colaborador("Juan",formasDeColaborarEnumDONADORTRANSPORTADOR);
+
+    assertEquals(formasDeColaborarEnumDONADORTRANSPORTADOR,Juan.getListaFormaDeColaborarEnum(),"Colabora como Donador y transportador");
+  }
+}
